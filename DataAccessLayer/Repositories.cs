745510a24cb8598d48fee51b156467719f035260
beasonsoles <99135550+beasonsoles@users.noreply.github.com>
2023-03-07 23:13:41 -0800

    Uploading base files

---
+++ DataAccessLayer/Repositories.cs
@@ -0,0 +1,23 @@
+﻿namespace DataAccessLayer
+{
+    public interface IStandardRepository : IRepository<Standard>
+    {
+    }
+
+    public interface IStudentRepository : IRepository<Student>
+    {
+    }
+
+    public class StandardRepository : Repository<Standard>, IStandardRepository
+    {
+    }
+
+    public class StudentRepository : Repository<Student>, IStudentRepository
+    {
+    }
+
+    public StudentRepository()
+    : base(new SchoolDBEntities())
+    {
+    }
+}
